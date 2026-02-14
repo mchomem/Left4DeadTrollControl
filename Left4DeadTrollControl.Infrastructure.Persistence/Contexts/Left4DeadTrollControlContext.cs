6b64d74f8a489e0fc4353ff32a6c55b2c129a14d
Misael da Costa Homem <misael.homem@gmail.com>
2026-02-14 12:10:09 -0300

    chore: Removal of logs for analysis.

--- Left4DeadTrollControl.Infrastructure.Persistence/Contexts/Left4DeadTrollControlContext.cs
+++ Left4DeadTrollControl.Infrastructure.Persistence/Contexts/Left4DeadTrollControlContext.cs
@@ -2,31 +2,12 @@
 
 public class Left4DeadTrollControlContext : DbContext
 {
-    private static int _instanceCount = 0;
-    private readonly int _instanceId;
-
     public DbSet<TrollPlayer> Trolls { get; set; }
 
-    public Left4DeadTrollControlContext(DbContextOptions<Left4DeadTrollControlContext> options) : base(options) 
-    {
-        _instanceId = System.Threading.Interlocked.Increment(ref _instanceCount);
-        System.Diagnostics.Debug.WriteLine($"[DbContext #{_instanceId}] CRIADO");
-    }
+    public Left4DeadTrollControlContext(DbContextOptions<Left4DeadTrollControlContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-    }
-
-    public override void Dispose()
-    {
-        System.Diagnostics.Debug.WriteLine($"[DbContext #{_instanceId}] DESCARTADO");
-        base.Dispose();
-    }
-
-    public override async ValueTask DisposeAsync()
-    {
-        System.Diagnostics.Debug.WriteLine($"[DbContext #{_instanceId}] DESCARTADO (Async)");
-        await base.DisposeAsync();
     }
 }
